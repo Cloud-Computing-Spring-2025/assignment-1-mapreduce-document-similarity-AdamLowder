de3f985e952c8153e4039ec0f00c86e23c245248
AdamLowder <alowde15@charlotte.edu>
2025-02-14 22:48:33 +0000

    Assignment1

--- src/main/java/com/example/DocumentSimilarityMapper.java
+++ src/main/java/com/example/DocumentSimilarityMapper.java
@@ -1,5 +1,39 @@
 package com.example;
 
-public class DocumentSimilarityMapper  {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+public class DocumentSimilarityMapper extends Mapper<Object, Text, Text, Text> {
+    private Text docPair = new Text();
+    private Text word = new Text();
     
+    @Override
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        //retrieve the current document's name
+        String fileName = ((org.apache.hadoop.mapreduce.lib.input.FileSplit) context.getInputSplit()).getPath().getName();
+        //document content is the value passed from the input split
+        String docContent = value.toString().trim();
+        //split document content into words
+        String[] wordsArray = docContent.split("\\s+");
+        //create a set of words from the document content
+        HashSet<String> words = new HashSet<>();
+        for (String word : wordsArray){
+            words.add(word.trim().toLowerCase());
+        }
+        //emit pairs of documents for each word
+        for (String word : words){
+            word.set(word);
+            for (String otherDocName : words){
+                //only emit pairs of different documents
+                if (!otherDocName.equals(fileName)){
+                    docPair.set(fileName + "," + otherDocName); 
+                    context.write(docPair, word);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/example/DocumentSimilarityReducer.java
+++ src/main/java/com/example/DocumentSimilarityReducer.java
@@ -1,5 +1,34 @@
 package com.example;
 
-public class DocumentSimilarityReducer {
-    
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class DocumentSimilarityReducer extends Reducer<Text, Text, Text, Text> {
+
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        //split the document pair
+        String[] docs = key.toString().split(",");
+        String doc1 = docs[0];
+        String doc2 = docs[1];
+        //create HashSets for intersection and union
+        HashSet<String> intersection = new HashSet<>();
+        HashSet<String> union = new HashSet<>();
+        Iterator<Text> wordIterator = values.iterator();
+        //iterate over the words for the document pair
+        while (wordIterator.hasNext()){
+            String word = wordIterator.next().toString();
+            intersection.add(word);
+            union.add(word);
+        }
+        //jaccard Similarity
+        double similarity = (double) intersection.size() / union.size();
+        //emit the result
+        context.write(new Text(doc1 + ", " + doc2), new Text("Similarity: " + similarity));
+    }
 }
