c5fe8170ea9de46dd6937f604c0c41c3eacfcc81
AdamLowder <alowde15@charlotte.edu>
2025-02-15 04:26:44 +0000

    Assignment1

--- src/main/java/com/example/controller/DocumentSimilarityDriver.java
+++ src/main/java/com/example/controller/DocumentSimilarityDriver.java
@@ -7,6 +7,9 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import com.example.DocumentSimilarityMapper;
+import com.example.DocumentSimilarityReducer;
+
 public class DocumentSimilarityDriver {
     
     public static void main(String[] args) throws Exception {
@@ -20,8 +23,8 @@ public class DocumentSimilarityDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         //set the input and output paths
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[0], args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         //run the job
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
